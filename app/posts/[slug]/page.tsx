b4c77ef5fe5352832ecd25df5cf716c6372c73c7
Mateis <mateis.pro@outlook.fr>
2025-02-18 22:18:56 +0100

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,13 +1,21 @@
 import { getPost } from "../../lib/posts";
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
-  const post: any = await getPost(params.slug);
+const PostPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+	const resolvedParams = await params;
 
-  return (
-    <article className="p-6">
-      <h1 className="text-3xl font-bold">{post.title}</h1>
-      <p className="text-gray-500">{post.date}</p>
-      <div className="mt-4 prose" dangerouslySetInnerHTML={{ __html: post.content }} />
-    </article>
-  );
+	const post: any = await getPost(resolvedParams.slug);
+
+	if (!resolvedParams.slug) {
+		return <p>[ERROR]: Something went wrong.</p>
+	}
+
+	return (
+		<article className="p-6">
+			<h1 className="text-3xl font-bold">{post.title}</h1>
+			<p className="text-gray-500">{post.date}</p>
+			<div className="mt-4 prose" dangerouslySetInnerHTML={{ __html: post.content }} />
+		</article>
+	);
 }
+
+export default PostPage;
